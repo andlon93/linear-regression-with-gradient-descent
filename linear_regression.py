2917eb9c97302951a496be59994699ed490e198e
andlon93 <andreas.lones@gmail.com>
2016-01-26 18:02:05 +0100

    reading input from file

--- linear_regression.py
+++ linear_regression.py
@@ -1,3 +1,18 @@
-fil=open("data-train.csv","r")
-for line in fil:
-	print(line)
+# --- Function that read data from file
+#     Input: type e.g. train or test
+#     Output: lists with inputs and outputs
+def read_file(type):
+	fil=open("data-"+type+".csv","r")
+	x1=[]
+	x2=[]
+	y=[]
+	for line in fil:
+		_x1,_x2,_y = line.strip().split(",")
+		x1.append(float(_x1))
+		x2.append(float(_x2))
+		y.append(float(_y))
+	fil.close()
+	return x1, x2, y
+#
+x1_train, x2_train, y_train = read_file("train")
+x1_test, x2_test, y_test = read_file("test")
